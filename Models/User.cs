7163ec72231c9b5151053c1cc12d58adbab475ed
n2m2912 <mannguyen281129@gmail.com>
2021-06-14 22:42:01 +0700

    The User Model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace DOTNET_RPG.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
