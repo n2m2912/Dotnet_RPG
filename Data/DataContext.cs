d27e9ccc33935c2284eb926577d7994390883a7f
n2m2912 <mannguyen281129@gmail.com>
2021-06-19 12:47:13 +0700

    One-To-One Relationship with Weapons

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,5 +13,7 @@ namespace DOTNET_RPG.Data
         public DbSet<Character> Characters { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
